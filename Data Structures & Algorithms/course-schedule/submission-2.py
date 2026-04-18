7ef3d07475dd1f7d88c079f0814881b9e5ac695c
Natan <135863264+natanaelo-jr@users.noreply.github.com>
2026-04-18 15:29:09 -0300

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [set() for _ in range(numCourses)]
+        for prerequisite in prerequisites:
+            course = prerequisite[0]
+            dependency = prerequisite[1]
+            graph[course].add(dependency)
+
+        visited = set()
+        checked = set()
+        isValid = [True]
+        
+        def dfs(course_index, checked, valid):
+            if valid[0] == False:
+                return
+            if course_index in checked:
+                valid[0] = False
+                return 
+
+            if course_index in visited:
+                return
+            
+            checked.add(course_index)
+            for dependency in graph[course_index]:
+                dfs(dependency, checked, valid)
+            checked.remove(course_index)
+            visited.add(course_index)
+            return
+
+        for i in range(len(graph)):
+            dfs(i, checked, isValid)
+
+        return isValid[0]
